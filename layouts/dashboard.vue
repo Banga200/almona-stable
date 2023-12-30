790f4bc0ccac79495c4693b629641a648eb634cc
Mohammed Salem Bankaita <mohammed.bankaita@gmail.com>
2023-12-30 16:36:31 +0300

    تعديل الناف في لوحة التحكم

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { ref } from 'vue'
 import { useUserStore } from '~/stores/auth';
-
+import {useDisplay} from 'vuetify'
 const userStore = useUserStore();
 const drawer = ref(false);
-const openPasswordDialog = ref(false)
+const openPasswordDialog = ref(false);
+const {mobile} = useDisplay();
 const items = ref([
     {
         title: "الطلبات",
@@ -38,9 +39,9 @@ function changePasswordDialogState(value) {
                 <v-row class="align-center">
 
                     <v-btn to="/" stacked>الصفحة الرئيسية</v-btn>
-                    <v-btn to="/real-estate/عقارات" stacked> عقارات</v-btn>
+                    <v-btn to="/real-estate/عقارات" stacked v-if="!mobile"> عقارات</v-btn>
                     <v-spacer></v-spacer>
-                    <v-btn stacked prepend-icon="mdi-plus" to="/real-estate/advertis">إضافة إعلان</v-btn>
+                    <v-btn stacked prepend-icon="mdi-plus" to="/real-estate/advertis" v-if="!mobile">إضافة إعلان</v-btn>
                     <v-spacer></v-spacer>
                     <user-drop-menu v-if="isUser" :User="isUser" @PasswordDialog="changePasswordDialogState" />
                 </v-row>
@@ -56,7 +57,12 @@ function changePasswordDialogState(value) {
             <v-list>
                 <v-list-group>
                     <v-list-item v-for="item in items" :key="item.title" :prepend-icon="item.icon" :to="item.link"
-                        color="primary" clas style="padding-inline: 20px !important;">{{ item.title }}</v-list-item>
+                        color="primary"  style="padding-inline: 20px !important;">{{ item.title }}</v-list-item>
+                        <v-divider class="mt-5"/>
+                        <v-list-item 
+                        color="primary"  style="padding-inline: 20px !important;" to="/real-estate/عقارات" >عقارات</v-list-item>
+                        <v-list-item 
+                        color="primary"  style="padding-inline: 20px !important;" to="/real-estate/advertis" append-icon="mdi-plus"> إضافة إعلان </v-list-item>
                 </v-list-group>
             </v-list>
         </v-navigation-drawer>
